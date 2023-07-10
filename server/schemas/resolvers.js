39690211fb66d2bc528e695d24fa6c647513afd4
ramasantayana <rama.santayana@uconn.edu>
2023-07-10 06:59:16 -0400

    Updated resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,7 +14,17 @@ const resolvers = {
         throw new AuthenticationError('Not logged in');
       },
     },
-  
+    Mutation: {
+        createUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+    
+    },
+
+
   };
   
   module.exports = resolvers;
