e31f9bb919012735e5645c16104d1f29ae70eda4
ramasantayana <rama.santayana@uconn.edu>
2023-07-10 06:51:53 -0400

    Updated typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,6 +9,17 @@ const typeDefs = gql`
     savedBooks: [Book]
   }
 
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+
 `;
+
+
 
 module.exports = typeDefs;
