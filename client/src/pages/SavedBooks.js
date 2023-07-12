a31abdcafee30c6e53a51b860738e2b2d7fca631
ramasantayana <rama.santayana@uconn.edu>
2023-07-12 06:39:10 -0400

    Updated SavedBBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -9,6 +9,9 @@ import {
 
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
